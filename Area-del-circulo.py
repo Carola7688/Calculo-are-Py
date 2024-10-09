9216c44c69264473bd4edb8544052f7bd56bd811
Carola <alvarezcarolina838@gmail.com>
2024-10-09 01:07:39 -0300

    Realizamos una calculadora de área basada en el radio de un circunferencia para practicar algoritmos, pseudogodigo y diagrama de flujos

--- Area-del-circulo.py
+++ Area-del-circulo.py
@@ -1,10 +1,13 @@
+
 # Vamos a calcular del area de un circulo, segun el radio ingresado por el usuario
 
 import math
 
-
+#Paso 1: Solicitar al usuario que ingrese el radio del circulo
 Radio = float(input("Ingrese el radio de la circunferencia: "))
 
+# Paso 2: Calcular el área del circulo utilizando la fórmula: área = π * radio^2
 Area = math.pi * (Radio**2)
 
+# Paso 3: Mostrar al usuario el área calculada
 print("El area de la circunferencia de radio", Radio, "es: ", Area)
